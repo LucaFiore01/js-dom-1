0390a3653a5b3dd0163541099182deecb97cde0a
Luca Fiore <lf44551@gmail.com>
2025-12-10 14:04:15 +0100

    feat: aggiunta pulsante "Spegni" per la gestione della lampadina con le varie funzioni

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Parte di preparazione
 // selezione elemento (bottone)
 const button = document.querySelector('.bottone')
+const offButton = document.querySelector('.bottone-off')
 // selezione elemento (lampadina)
 const offLamp = document.querySelector('.lampadina-spenta')
 const onLamp = document.querySelector('.lampadina-accesa')
@@ -10,6 +11,8 @@ button.addEventListener('click',
     function () {
       onLamp.classList.add('show')
       offLamp.classList.add('hide')
+      offButton.classList.add('show')
+      button.classList.add('hide')
     }
 )
 
